c1497068df24903927c9c550483890e8491309ea
Philip Meholm <philip.meholm@gmail.com>
2016-12-17 17:00:28 +0100

    //

--- Json.Extended.psm1
+++ Json.Extended.psm1
@@ -1,9 +1,51 @@
-﻿try
+﻿function Test-Administrator  
+{  
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+
+Function Get-Newtonsoft.Json
+{
+    if(!(Test-Administrator))
+    {
+        Start-Process powershell -ArgumentList "-noexit","-Command  `"&{`Install-Package Newtonsoft.Json -Force`"}`"" -Verb runas -Wait
+    }
+    else
+    {
+        Install-Package Newtonsoft.Json -Force
+    }
+}
+
+Function Get-Newtonsoft.Json.Schema
+{
+    if(!(Test-Administrator))
+    {
+        Start-Process powershell -ArgumentList "-noexit","-Command  `"&{`Install-Package Newtonsoft.Json.Schema`"}`"" -Verb runas -Wait
+    }
+    else
+    {
+        Install-Package Newtonsoft.Json.Schema -Force
+    }
+}
+
+try
 {
     Import-Module newtonsoft.json -ErrorAction Stop
+    Write-Verbose "Newtonsoft.json Imported!"
 }
 catch
 {
-    Throw $_
-    if($script:Admin )
+    Write-Error "Newtonsoft.json Is not installed on this system! Use 'Get-Newtonsoft.Json' to install"
+    #Write-Error $_
+}
+
+try
+{
+    Import-Module Newtonsoft.Json.Schema -ErrorAction Stop
+    Write-Verbose "Newtonsoft.Json.Schema Imported!"
+}
+catch
+{
+    Write-Error "Newtonsoft.Json.Schema Is not installed on this system! Use 'Get-Newtonsoft.Json.Schema' to install"
+    #Write-Error $_
 }
---
+++ Scripts/Schema.ps1
@@ -0,0 +1,38 @@
+﻿$script:SchemaDLLPath = $(Get-ChildItem "$(Split-Path $PSScriptRoot -Parent)\DLL\Schema" -Filter '*.dll')
+$script:JsonDLLPath = $(Get-ChildItem "$(Split-Path $PSScriptRoot -Parent)\DLL\Json" -Filter '*.dll')
+
+
+Function Add-SchemaDll
+{
+    #$script:SchemaDLL = add-type -Path $script:SchemaDLLPath.FullName
+    try
+    {
+        #$NewtonsoftJsonDllPath = Get-ChildItem (Get-Module newtonsoft.json).ModuleBase -Recurse -Filter *.dll
+        #$asm = [Reflection.Assembly]::LoadFile("$PSScriptRoot\libs\Newtonsoft.Json.dll")
+        $asm = [reflection.assembly]::LoadFrom($script:JsonDLLPath.FullName)
+        Add-Type -Path $script:SchemaDLLPath.FullName -ReferencedAssemblies $script:JsonDLLPath.FullName
+    }
+    catch
+    {
+        $_.Exception.LoaderExceptions | %{Write-Error $_.Message}
+    }
+}
+
+Function convertto-JSchema
+{
+    param(
+        $inputobject
+    )
+    Add-SchemaDll
+    #$inputobject
+    #Write-Host "$([System.Type]::GetType($inputobject))"
+    $JGenerator = [Newtonsoft.Json.Schema.Generation.JSchemaGenerator]::new()
+    $jschema = $JGenerator.Generate($inputobject.gettype())
+    $jschema
+}
+
+function New-JschemaObject
+{
+    
+}
+
